103487d3f4f6927767e6275723c2b4faf79ab592
AI-Amponsah <Yawamp27@gmail.com>
2022-12-08 00:02:51 +0000

    PID

---
+++ Shell/Process_ID.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+
+/**
+ * main 
+ * Return: Always 0
+*/
+
+int main(void)
+{
+    pid_t my_pid;
+    my_pid = getpid();
+    printf("%d\n", my_pid);
+    return (0);
+}
